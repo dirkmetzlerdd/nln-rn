3f1c0ac1e1e29ba87d8365bb0f3720859771f725
Dirk Metzler <dirkmetzler@icloud.com>
2023-05-05 19:51:58 +0200

    Improve style in newsDetails

--- screens/newsDetails.tsx
+++ screens/newsDetails.tsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import {
   View,
   ScrollView,
@@ -7,30 +6,14 @@ import {
   Image,
   Text,
 } from "react-native";
-import { collection, onSnapshot } from "firebase/firestore";
-import { initialize } from "../firebase/main";
-import { DB_COLS } from "../types/main";
-import { News } from "../types/news";
 import { wrapAsHtml } from "../utils/main";
 import AutoHeightWebView from "react-native-autoheight-webview";
 import { StackScreenProps } from "../types";
 
-const { firestore } = initialize();
-
 export default function NewsDetails({
   route,
 }: StackScreenProps<"NewsDetails">) {
-  const [newsData, setNewsData] = useState<Array<News>>([]);
   const { news } = route?.params;
-
-  useEffect(() => {
-    onSnapshot(collection(firestore, DB_COLS.news), (snapshot) => {
-      const result: Array<News> = snapshot.docs.map((item) => {
-        return { id: item.id, ...item.data() } as News;
-      });
-      setNewsData(result);
-    });
-  }, []);
 
   return (
     <View style={styles.container}>
@@ -60,12 +43,14 @@ export default function NewsDetails({
 
 const styles = StyleSheet.create({
   container: { flex: 1 },
-  contentWrapper: { padding: 15, marginBottom: 15 },
+  contentWrapper: {
+    paddingHorizontal: 15,
+    marginBottom: 15,
+  },
   header: {
     paddingVertical: 10,
     textTransform: "uppercase",
-    fontWeight: "bold",
-    fontSize: 22,
+    fontSize: 24,
   },
   image: {
     width: "100%",
